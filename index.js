e2eb8960098c73a23711c609aa3658dd530964bd
Alan Bignon <43960612+AlanBignon@users.noreply.github.com>
2019-01-23 12:07:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,33 @@ function hide(div) {
     document.getElementById(div).style.display='none';
     return false;
 }
+
+
+
+function surligne(champ, erreur) {
+
+    if(erreur)
+        champ.style.backgroundColor = "#fba";
+    else
+        champ.style.backgroundColor = "";
+}
+
+
+function verifMail(champ) {
+
+    let regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
+
+    if(!regex.test(champ.value))
+
+    {
+        surligne(champ, true);
+        return false;
+    }
+
+    else
+    {
+        surligne(champ, false);
+        return true;
+
+    }
+}
