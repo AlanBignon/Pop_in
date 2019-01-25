de9a96aa7b05ea93e87bf7eb7861d8bc83ab9249
Alan Bignon <43960612+AlanBignon@users.noreply.github.com>
2019-01-25 16:24:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,92 @@
-function pop(div) {
-    document.getElementById(div).style.display='block';
+function show() {
+    document.getElementById('pop').style.display='block';
     return false;
 }
-function hide(div) {
-    document.getElementById(div).style.display='none';
+function hide() {
+    document.getElementById('pop').style.display='none';
     return false;
 }
+function init()
+{
+    let oA = document.getElementById('button');
 
+    oA.onclick = function()
+    {
+        verifMail();
+        verifPassword();
+        return false;
+    };
+}
 
-
-function surligne(champ, erreur) {
+function surligne(erreur) {
 
     if(erreur)
-        champ.style.backgroundColor = "#fba";
+    {
+        document.getElementById('email').style.backgroundColor = "#fba";
+        document.getElementById('alerte').innerHTML = "Vos renseignement sont invalides."
+    }
     else
-        champ.style.backgroundColor = "";
+    {
+        document.getElementById('email').style.backgroundColor = "#00FF00";
+    }
+}
+
+function verifPassword() {
+    let pass = document.getElementById('password').value;
+
+    if (pass === "")
+    {
+        document.getElementById('alerte').innerHTML = "Veuillez rentrez un mot de passe.";
+        return false;
+    }
+    else
+    {
+        if (verifMail())
+        {
+            document.getElementById('alerte').innerHTML = "Vos renseignement sont valides.";
+        }
+    }
 }
 
 
-function verifMail(champ) {
+function verifMail() {
 
     let regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
 
-    if(!regex.test(champ.value))
+    if(!regex.test(document.getElementById('email').value))
 
     {
-        surligne(champ, true);
+        surligne(true);
         return false;
     }
-
     else
     {
-        surligne(champ, false);
+        surligne (false);
         return true;
-
     }
 }
+
+function close()
+{
+    let set = document.getElementById('close');
+
+    set.onclick = function()
+    {
+        hide();
+        return false;
+    };
+}
+
+function open()
+{
+    let set = document.getElementById('open');
+
+    set.onclick = function()
+    {
+        show();
+        return false
+    };
+}
+
+
+window.onload = function() { init(); open(); close() };
